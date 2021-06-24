e76a818a4316f1e9a787a8bddca8b0b07ea88c80
harmoniahj <harmoniahj@naver.com>
2021-06-24 16:15:03 +0900

    06월 24일 pull 받음 + Admin 추가

---
+++ final_spring_1/src/admin/mvc/AdminLogic.java
@@ -0,0 +1,13 @@
+package admin.mvc;
+
+import org.apache.log4j.Logger;
+
+public class AdminLogic {
+	Logger logger = Logger.getLogger(AdminLogic.class);
+	
+	private AdminDao adminDao = null;
+	
+	public int outMember(String memEmail) {
+		return 0;
+	}
+}
