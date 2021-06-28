a09a59ad36e935fe8427ed9e55e15eea8b0c5191
eunyoungkim <snu9451@naver.com>
2021-06-28 19:03:05 +0900

    2021 06 28 월 19:02 작성자: 김은영 메인페이지와 Back 연결 작업 중 clean한 상태의 프로젝트입니다. 받으세요.

--- final_spring_1/WebContent/mainPage_JSP/assets/js/main_page.js
+++ final_spring_1/WebContent/mainPage_JSP/assets/js/main_page.js
@@ -46,11 +46,6 @@
     );
   });
 
-  //회원가입 모달창 이벤트들
-  $(document).ready(function () {
-    
-  });
-
   //지도 말풍선 클릭시 이벤트
   $(window).on("load", function () {
     console.log(3, "window.onload");
@@ -108,9 +103,6 @@
       $("#report_content_user").submit();
     });
   });
-
-  //drop down toggle
-  $('.dropdown-toggle').dropdown('toggle');
 
   /*==========================================[[ Not Mine ]]==================================================*/
 
