6fa8c9921b8de6083e3e23ef0706e6d31f0d096d
jks5117 <jks5117@naver.com>
2021-07-14 21:41:57 +0900

    2021 07 14 조기설 병합한거 가지고 오기

--- sms/coolsms-java/model/src/main/java/request/ImageModel.java
+++
@@ -1,9 +0,0 @@
-package model.request;
-
-public class ImageModel {
-    public ImageModel(String base64encoded) {
-        file = base64encoded;
-    }
-    String file;
-    String type = "MMS";
-}
