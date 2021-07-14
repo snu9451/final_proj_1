6d7ed7b6dde5955bcb1bfc2c75e0c8eb69f1b3e8
amazingtmon <webtree12@gmail.com>
2021-07-14 21:15:04 +0900

    2021 07 14 merge 파일 받기전 폴더 비우기

--- sms/coolsms-java/app/src/main/java/model/response/ImageResult.java
+++
@@ -1,9 +0,0 @@
-package model.response;
-
-public class ImageResult {
-    String fileId = "";
-
-    public String getImageId() {
-        return fileId;
-    }
-}
--- sms/coolsms-java/model/src/main/java/response/DeleteImageResult.java
+++
@@ -1,9 +0,0 @@
-package model.response;
-
-public class DeleteImageResult {
-    String result;
-
-    public String getResult() {
-        return result;
-    }
-}
